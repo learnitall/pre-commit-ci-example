99a68d86f00b24ce4f260ac50bef37f9791c61e9
Ryan Drew <learnitall0@gmail.com>
2021-08-10 12:15:39 -0600

    Add bad python file
    Used no-verify

---
+++ bad.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Bad python file that will be autofixed."""
+
+
+def main():
+    my_string = "this is my really really really really really really really really really really really really really really really really really really really really really really long string."
+
+    my_set = set([1, 2, 3, 4])
+
+    print("Hey check out this string: %s" % my_string)
+
+
+main()
