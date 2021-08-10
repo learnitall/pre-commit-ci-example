97bfb8177a159b8bf929c381566a196c0cefe8fa
Ryan Drew <learnitall0@gmail.com>
2021-08-10 12:13:08 -0600

    Add good python file

---
+++ good.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Simple Python module that passes all lint checks."""
+
+
+def main() -> None:
+    """Print "Hello World" and exit."""
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    main()
