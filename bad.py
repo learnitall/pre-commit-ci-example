921deec873a0ae9c909319e13ff9982b6286a0b3
Ryan Drew <learnitall0@gmail.com>
2021-08-19 10:50:36 -0600

    Replace spaces with tabs

--- bad.py
+++ bad.py
@@ -4,20 +4,20 @@
 
 
 def main():
-    my_string = "this is my really really really really really really really really really really really really really really really really really really really really really really long string."
+	my_string = "this is my really really really really really really really really really really really really really really really really really really really really really really long string."
 
-    my_set = {1, 2, 3, 4}
+	my_set = {1, 2, 3, 4}
 
-    def this_is_a_function_with_lots_of_args(
-        arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15
-    ):
-        print(
-            "I ran this really long function with a lot of args: {} {} {} {} {} {}".format(
-                arg1, arg2, arg3, arg4, arg5
-            )
-        )
+	def this_is_a_function_with_lots_of_args(
+	    arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15
+	):
+	    print(
+	        "I ran this really long function with a lot of args: {} {} {} {} {} {}".format(
+	            arg1, arg2, arg3, arg4, arg5
+	        )
+	    )
 
-    print("Hey check out this string: %s" % my_string)
+	print("Hey check out this string: %s" % my_string)
 
 
 main()
